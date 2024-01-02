2447e03bf0c5389a5d43b468f1e5934dbc1e5f4c
Xqhare <xqhare@web.de>
2024-01-02 22:01:10 +0100

    added ping function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::net::IpAddr;
+use std::str::FromStr;
+use std::time::Duration;
+
+use neith::Neith;
+
 fn main() {
-    println!("Hello, world!");
+    // DB set up
+    let mut con = Neith::connect("shamash");
+    let _table = con.execute("new table uptime with (id true, time false, up_bool false)");
+
+}
+
+fn ping() -> Result<(), ping::Error> {
+    let dur = Some(Duration::from_secs(2));
+    // This needs sudo to work!!! so cargo run won't!
+    let addr = IpAddr::from_str("209.85.233.101").unwrap();
+    // let addr = "209.85.233.101:80".parse::<IpAddr>().unwrap();
+    ping::ping(addr, dur, None, None, None, None)
 }
