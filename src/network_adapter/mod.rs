d0b7e8605f16f2b6649bcfe25434efc964bdf216
Xqhare <xqhare@web.de>
2024-10-17 00:29:31 +0200

    feat: finished rewrite with several networks in mind

--- src/network_adapter/mod.rs
+++ src/network_adapter/mod.rs
@@ -1,26 +1,58 @@
-use std::collections::{BTreeMap, VecDeque};
+use std::{collections::{BTreeMap, VecDeque}, thread, time::Duration};
 
 use sysinfo::Networks;
 
+use crate::MEASUREMENT_INTERVAL;
+
+
+#[derive(Clone, Debug)]
+/// Network traffic handler
+/// Used to store and update network traffic of all adapters
+/// Always constructed with new
+/// Call update() periodically
 pub struct NetworkTrafficHandler {
     pub load_map: BTreeMap<String, VecDeque<u64>>,
+    pub thread_spawned_map: BTreeMap<String, bool>,
+    pub internet_restored_map: BTreeMap<String, bool>,
     last_received_map: BTreeMap<String, u64>,
 }
 
 impl NetworkTrafficHandler {
-    pub fn new() -> Self {
-        let networks = Networks::new_with_refreshed_list();
+    /// Create new network traffic handler with a wait time in milliseconds
+    pub fn new(wait_time: u64) -> Self {
+        let mut networks = Networks::new_with_refreshed_list();
 
         let mut last_received_map = BTreeMap::new();
+        let mut thread_spawned_map = BTreeMap::new();
+        let mut internet_restored_map = BTreeMap::new();
 
         for network in networks.iter() {
             let name = network.0.to_string();
+            if name == "lo" { continue; }
             let last_received = network.1.total_received();
-            last_received_map.insert(name, last_received);
+            last_received_map.insert(name.clone(), last_received);
+            thread_spawned_map.insert(name.clone(), false);
+            internet_restored_map.insert(name, false);
+        }
+
+        // wait for refresh to have new data
+        thread::sleep(Duration::from_millis(wait_time));
+        networks.refresh();
+
+        let mut load_map: BTreeMap<String, VecDeque<u64>> = BTreeMap::new();
+
+        for network in networks.iter() {
+            let name = network.0.to_string();
+            if name == "lo" { continue; }
+            let now_received = network.1.total_received();
+            let load = now_received.saturating_sub(*last_received_map.get(&name).expect("No last value!"));
+            load_map.insert(name, VecDeque::from([load]));
         }
 
         Self {
-            load_map: BTreeMap::new(),
+            load_map,
+            thread_spawned_map,
+            internet_restored_map,
             last_received_map
         }
     }
@@ -32,19 +64,13 @@ impl NetworkTrafficHandler {
             let now_received = network.1.total_received();
             let last_received = self.last_received_map.insert(name.clone(), now_received).expect("No value to update!");
             let load = now_received.saturating_sub(last_received);
-            if self.load_map.len() == 60 {
-                if let Some(load_vec) = self.load_map.get_mut(&name) {
-                    load_vec.pop_front();
-                    load_vec.push_back(load);
-                } else {
-                    panic!("No value to update!");
-                }
+            if self.load_map.get(&name).expect("No value found!").len() == MEASUREMENT_INTERVAL {
+                let load_vec = self.load_map.get_mut(&name).expect("No value to update!");
+                load_vec.pop_front();
+                load_vec.push_back(load);
             } else {
-                if let Some(load_vec) = self.load_map.get_mut(&name) {
-                    load_vec.push_back(load);
-                } else {
-                    panic!("No value to update!");
-                }
+                let load_vec = self.load_map.get_mut(&name).expect("No value to update!");
+                load_vec.push_back(load);
             }
         }
     }
