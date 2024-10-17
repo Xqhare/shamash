2723fdf591ae0b1d420a8ed94b414b523fb165f3
Xqhare <xqhare@web.de>
2024-10-17 18:52:32 +0200

    feat: added return, debug println now only prints in non release mode

--- src/no_internet.rs
+++ src/no_internet.rs
@@ -6,7 +6,9 @@ use time::OffsetDateTime;
 
 use crate::{network_adapter::NetworkTrafficHandler, INTERNET_RESTORED_THRESHOLD, MEASUREMENT_INTERVAL, SHORT_WAIT_TIME, STORAGE_DIR};
 
-pub fn no_internet(term_now: Arc<AtomicBool>, adapter_name: String, load_map: BTreeMap<String, VecDeque<u64>>) {
+/// While returning a result, this function will always panic instead of error, and return `Ok(())`
+/// if internet has returned
+pub fn no_internet(term_now: Arc<AtomicBool>, adapter_name: String, load_map: BTreeMap<String, VecDeque<u64>>) -> Result<(), Box<dyn std::error::Error>> {
 
     // logging data
     let date_time_of_incident = OffsetDateTime::now_utc();
@@ -24,7 +26,9 @@ pub fn no_internet(term_now: Arc<AtomicBool>, adapter_name: String, load_map: BT
         for (name, last_interval_incoming) in new_network_handler.load_map.iter() {
             if name == &adapter_name && last_interval_incoming.len() == MEASUREMENT_INTERVAL {
                 if last_interval_incoming.iter().sum::<u64>() >= INTERNET_RESTORED_THRESHOLD {
+                    #[cfg(debug_assertions)]
                     println!("Internet restored! {}", last_interval_incoming.iter().sum::<u64>());
+                    #[cfg(debug_assertions)]
                     println!("Interval: {:?}", last_interval_incoming);
                     break 'outer;
                 }
@@ -43,14 +47,17 @@ pub fn no_internet(term_now: Arc<AtomicBool>, adapter_name: String, load_map: BT
     let end_date = date_time_end_of_incident.date();
     let end_time = date_time_end_of_incident.time();
     
+    #[cfg(debug_assertions)]
     let tmp = format!("Incident Report:
     Start: {start_date} {start_time}
     End: {end_date} {end_time}
     Duration: {duration_of_incident}ms");
+    #[cfg(debug_assertions)]
     println!("{}", tmp);
     
     // create log
     let xff_value = XffValue::from(vec![("start-date", format!("{}", start_date)), ("start-time", format!("{}", start_time)), ("end-date", format!("{}", end_date)), ("end-time", format!("{}", end_time)), ("duration", format!("{}", duration_of_incident))]);
+
     let mut log_dir_path = PathBuf::from(STORAGE_DIR);
     log_dir_path.extend(vec![adapter_name]);
     if !log_dir_path.exists() {
@@ -58,6 +65,13 @@ pub fn no_internet(term_now: Arc<AtomicBool>, adapter_name: String, load_map: BT
     }
     let mut log_path = log_dir_path.clone();
     log_path.extend(vec![format!("{start_date}-{start_time}.xff")]);
-    write(log_path, xff_value).expect("Failed to write log file");
+
+    let out = write(log_path, xff_value);
+    if out.is_err() {
+        panic!("Failed to write log: {}", out.unwrap_err());
+    } else {
+        return Ok(());
+    }
 
 }
+
