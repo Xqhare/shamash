e77bfe18d69c65be2e6a0c12bf582f17395c63ec
Xqhare <xqhare@web.de>
2026-01-21 22:35:07 +0100

    add(config): rework how targets are stored, and add seven more to a total of 10

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,7 @@
 use std::{env, time::Duration};
 
 const ROUTER_IP: &str = "192.168.178.1";
+const TARGETS: [&str; 10] = ["1.1.1.1", "1.0.0.1", "8.8.4.4", "8.8.8.8", "9.9.9.9", "94.140.14.14", "94.140.15.15", "149.112.112.112", "208.67.222.222", "208.67.220.220"];
 
 pub struct Config {
     pub router_ip: String,
@@ -16,7 +17,7 @@ impl Config {
         let log_dir_path = env::var("LOG_DIR_PATH").unwrap_or_else(|_| "./shamash-logs/".to_string());
         Self {
             router_ip,
-            targets: vec!["1.1.1.1".to_string(), "8.8.8.8".to_string(), "9.9.9.9".to_string()],
+            targets: TARGETS.iter().map(|s| s.to_string()).collect(),
             interval_normal: Duration::from_secs(1),
             interval_recovery: Duration::from_millis(333),
             log_dir_path,
