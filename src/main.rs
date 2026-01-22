e7d04830ef914ebc259f59d44f9cd0c3c684aec9
Xqhare <xqhare@web.de>
2026-01-22 01:56:40 +0100

    add(main): rework to have the state handling moved into states/

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,24 @@
-use std::{
-    sync::{
-        atomic::{AtomicBool, Ordering},
-        Arc,
-    },
-    thread,
+use std::sync::{
+    atomic::{AtomicBool, Ordering},
+    Arc,
 };
 
 use config::Config;
 use horae::Utc;
 use log::Logger;
 use signal_hook::{consts::TERM_SIGNALS, flag};
-use utils::{is_answering_ping, ConnectionState};
+use states::{
+    diagnosing, 
+    isp_outage, 
+    local_outage,
+    online,
+    ConnectionState
+};
 
 mod config;
 mod log;
 mod utils;
+mod states;
 
 fn main() {
     let term_now = Arc::new(AtomicBool::new(false));
@@ -26,125 +30,30 @@ fn main() {
         }
     }
 
-    let config = Config::new();
+    let mut config = Config::new();
     let mut state = ConnectionState::Online;
-    let mut logger = Logger::new(config.log_dir_path);
-    let mut target_index = 0;
+    let mut logger = Logger::new(config.log_dir_path.clone());
 
     while !term_now.load(Ordering::Relaxed) {
-        let target = &config.targets[target_index];
-        target_index = next_index(target_index, config.targets.len());
+        config.iter_targets();
 
         match state {
             ConnectionState::Online => {
-                if is_answering_ping(target, config.interval_normal) {
-                    thread::sleep(config.interval_normal);
-                } else {
-                    let now = Utc::now();
-                    logger.add_log_line(format!("{}", now));
-                    logger.add_log_line(format!("Target '{}' failed to answer", target));
-                    logger.add_separator();
-                    state = ConnectionState::Diagnosing;
+                if let Some(new_state) = online(&config, &mut logger) {
+                    state = new_state;
                 }
             }
             ConnectionState::Diagnosing => {
-                let now = Utc::now();
-                logger.add_log_line(format!("{} - Diagnosing", now));
-                logger.add_separator();
-
-                if is_answering_ping(&config.router_ip, config.interval_recovery) {
-                    // Router up, external seems out - sanity check
-                    // Normal interval to give every benefit
-                    if is_answering_ping(target, config.interval_normal) {
-                        logger.clear();
-                        state = ConnectionState::Online;
-                        continue;
-                    } else {
-                        let now = Utc::now();
-                        logger.add_log_line(format!("Mr. President: A second target ({}) failed to answer - The local network is under attack.", target));
-                        logger.add_log_line(format!("Declaring ISP outage at {}", now));
-                        state = ConnectionState::IspOutage;
-                    }
-                } else {
-                    let now = Utc::now();
-                    logger.add_log_line(format!("Router is down"));
-                    logger.add_log_line(format!(
-                        "Declaring local outage at {} - Roll the Trucks!",
-                        now
-                    ));
-                    state = ConnectionState::LocalOutage;
-                }
-                logger.add_separator();
+                state = diagnosing(&mut config, &mut logger);
             }
             ConnectionState::IspOutage => {
-                if is_answering_ping(target, config.interval_recovery) {
-                    let now = Utc::now();
-                    logger.add_log_line(format!(
-                        "Connection established with target '{}' at {}",
-                        target, now
-                    ));
-                    let next_target =
-                        &config.targets[next_index(target_index, config.targets.len())];
-                    if is_answering_ping(&next_target, config.interval_recovery) {
-                        let now = Utc::now();
-                        logger.end_log(format!(
-                            "Connection established with second target '{}' at {}",
-                            next_target, now
-                        ));
-                        state = ConnectionState::Online;
-                    } else {
-                        // One target reports up, the other down - Should be unreachable, but I
-                        // guess we'll just run the loop again.
-                        continue;
-                    }
-                } else {
-                    thread::sleep(config.interval_recovery);
+                if let Some(new_state) = isp_outage(&config, &mut logger) {
+                    state = new_state;
                 }
             }
             ConnectionState::LocalOutage => {
-                if is_answering_ping(&config.router_ip, config.interval_recovery) {
-                    let now = Utc::now();
-                    logger.add_log_line(format!("Connection with Router established at {}", now));
-                    logger.add_log_line(format!(
-                        "Local Outage end declared - checking outside connection"
-                    ));
-                    if is_answering_ping(&target, config.interval_recovery) {
-                        let now = Utc::now();
-                        logger
-                            .add_log_line(format!("Outside test connection successful at {}", now));
-                        logger.end_log(format!("Local Outage end at {}", now));
-                        state = ConnectionState::Online;
-                    } else {
-                        logger.add_log_line(format!(
-                            "Outside test connection unsuccessful at {}",
-                            now
-                        ));
-                        logger.add_log_line(format!(
-                            "Retrying in {} seconds",
-                            config.interval_recovery.as_secs()
-                        ));
-                        thread::sleep(config.interval_recovery);
-                        if is_answering_ping(&target, config.interval_recovery) {
-                            let now = Utc::now();
-                            logger
-                                .end_log(format!("Outside test connection successful at {}", now));
-                            state = ConnectionState::Online;
-                        } else {
-                            let now = Utc::now();
-                            logger.add_log_line(format!(
-                                "Outside test connection unsuccessful at {}",
-                                now
-                            ));
-                            logger.add_log_line(format!(
-                                "Declaring ISP outage at {}, continuing the outage",
-                                now
-                            ));
-                            logger.add_separator();
-                            state = ConnectionState::IspOutage;
-                        }
-                    }
-                } else {
-                    thread::sleep(config.interval_recovery);
+                if let Some(new_state) = local_outage(&config, &mut logger) {
+                    state = new_state;
                 }
             }
         }
