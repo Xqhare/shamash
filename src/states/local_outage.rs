b65f1668661ca565e2b6ed0dd6ca8b72c2782ada
Xqhare <xqhare@web.de>
2026-01-27 19:38:45 +0100

    add(states/local): duration logging of local outage, remove 'local_outage' file at dir root at end of outage, fix logging of retry for external check only logging full seconds for the interval

--- src/states/local_outage.rs
+++ src/states/local_outage.rs
@@ -12,7 +12,7 @@ pub fn local_outage(config: &Config, logger: &mut Logger) -> Option<ConnectionSt
         let now = Utc::now();
         logger.add_log_line(format!("🟢 Connection with Router established at {}", now));
         logger.add_log_line(format!(
-            "🟢 Local Outage end declared - checking outside connection"
+            "🟢 Local Outage end declared, duration: {} seconds - checking outside connection", logger.log_start.elapsed().as_secs_f64()
         ));
         logger.add_small_separator();
         if is_answering_ping(&config.current_target(), config.interval_recovery, logger, ConnectionState::LocalOutage) {
@@ -22,6 +22,7 @@ pub fn local_outage(config: &Config, logger: &mut Logger) -> Option<ConnectionSt
             logger.add_large_separator();
             logger.end_log(format!("🟢 Local Outage end at {}", now));
             logger.add_large_separator();
+            let _ = std::fs::remove_file(logger.log_dir_path.clone() + "/local_outage");
             Some(ConnectionState::Online)
         } else {
             logger.add_log_line(format!(
@@ -31,7 +32,7 @@ pub fn local_outage(config: &Config, logger: &mut Logger) -> Option<ConnectionSt
             ));
             logger.add_log_line(format!(
                 "Retrying in {} seconds",
-                config.interval_recovery.as_secs()
+                config.interval_recovery.as_secs_f64()
             ));
             logger.add_small_separator();
             thread::sleep(config.interval_recovery);
@@ -42,6 +43,7 @@ pub fn local_outage(config: &Config, logger: &mut Logger) -> Option<ConnectionSt
                         &config.current_target(),
                         now
                     ));
+                let _ = std::fs::remove_file(logger.log_dir_path.clone() + "/local_outage");
                 Some(ConnectionState::Online)
             } else {
                 let now = Utc::now();
@@ -55,6 +57,8 @@ pub fn local_outage(config: &Config, logger: &mut Logger) -> Option<ConnectionSt
                 ));
                 logger.add_large_separator();
                 logger.event_type = EventType::IspOutage;
+                let _ = std::fs::remove_file(logger.log_dir_path.clone() + "/local_outage");
+                let _ = std::fs::write(logger.log_dir_path.clone() + "/isp_outage", []);
                 Some(ConnectionState::IspOutage)
             }
         }
